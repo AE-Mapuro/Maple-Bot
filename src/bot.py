4a08e117842f9ccb6ec7c6df302a62e6012e1034
Aurik Sarker <auriksarker@gmail.com>
2023-10-26 12:09:57 -0400

    Added check for message length, changed function structure slightly

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,18 @@
+from sys import path 
+path.append('../resources')
+
 import interactions
 from resources import env
 import random
 
+from typing import Any
+
 intents = interactions.Intents.DEFAULT | interactions.Intents.MESSAGE_CONTENT
 client = interactions.Client(intents=intents, description="Maple Bot")
 
+BOT_NAME = 'Grendel'
+# Bots are limited to messages 2000 characters or fewer
+BOT_MSG_LEN_LIM = 2000
 
 # First to indicate successful login
 @interactions.listen()
@@ -13,32 +21,48 @@ async def on_ready():
     print(f"We're online! We've logged in as {client.user.username}.")
 
 
-@interactions.slash_command(name='ask', description='Ask Grendel Bot a question',
+@interactions.slash_command(name='ask', description=f'Ask {BOT_NAME} a question',
                             scopes=env.discord_guild_id)
 @interactions.slash_option(
     name='question',
-    description="Question asked by the user",
+    description="Question asked by the user (text)",
     required=True,
     opt_type=interactions.OptionType.STRING
 )
 @interactions.slash_option(
-    name='number_of_question_marks',
-    description="Severity of question asked",
+    name='severity',
+    description="Severity of question asked (integer)",
     required=False,
     opt_type=interactions.OptionType.INTEGER
 )
-async def askGrendel(ctx: interactions.SlashContext, question: interactions.OptionType.STRING,
-                     number_of_question_marks: interactions.OptionType.INTEGER = 1):
+async def askCommand(ctx: interactions.SlashContext, question: str, severity: int = 1):
     responses = ['', 'No', 'Whyyyy', 'Nani', 'Jes', 'Nani Kore', 'Watashi wa Kotowaru', 'Plz', 'Plz No',
-                 'No Violins Plox', 'This is too difficult to answer', 'A\'grid is back!']
+                 'No Violins Plox', 'This is too difficult to answer', '\'agrid\'s back!']
     expressions = ['(ㆆ _ ㆆ)', '⊂(◉‿◉)つ', '•`_´•', 'ʕっ•ᴥ•ʔっ', '(•_•) ( •_•)>⌐■-■ (⌐■_■)', '(｡◕‿‿◕｡)', '(︶︹︶)',
                    '┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻', '(ง •̀_•́)ง', '(ノಠ益ಠ)ノ彡┻━┻', R'¯\_(ツ)_/¯']
 
-    print(f'Command Message - ID: {ctx.message_id} -> {ctx.message}')
-    # await ctx.send(f"{random.choice(responses)} {random.choice(expressions)}")
-    await ctx.send(f"{question}{'?' * number_of_question_marks}")
-    message = await ctx.send(f"{random.choice(responses)} {random.choice(expressions)}")
+    # Generate bot response - two messages: 
+    # reiteration of the question
+    # Before appending question marks, remove any question marks from the end of the input string
+    question = question.rstrip('?')
+    question_full: str = f"{question}{'?' * severity}"
+    # response (randomly generated)
+    response: str = f"{random.choice(responses)} {random.choice(expressions)}"
 
+    # Message to be sent when input question is too long
+    # TODO This may be moved outside function scope
+    bot_response_too_long = f'The question is too long and/or the severity is too high. {BOT_NAME} is limited to responses {BOT_MSG_LEN_LIM} characters or fewer.'
+    
+    print(f'Command Message - ID: {ctx.message_id} -> {ctx.message}')
+    
+    messages: list[Any] = []
+    if len(question_full) <= BOT_MSG_LEN_LIM:
+        messages.append( await ctx.send( question_full ) )
+        messages.append( await ctx.send( response ) )
+    else:
+        messages.append( await ctx.send( bot_response_too_long ) )
+
+    return
 
 
 # Start the bot
