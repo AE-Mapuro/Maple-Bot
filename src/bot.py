a9fba7f2fc594a39ccf2eab6b08588e0b58c4cc1
Eric Tsai <e.tsai70@gmail.com>
2023-10-25 22:00:56 -0400

    Adding additional testing commands

--- src/bot.py
+++ src/bot.py
@@ -2,8 +2,6 @@ import interactions
 from resources import env
 import random
 
-from interactions import Snowflake_Type
-
 intents = interactions.Intents.DEFAULT | interactions.Intents.MESSAGE_CONTENT
 client = interactions.Client(intents=intents, description="Maple Bot")
 
@@ -13,6 +11,7 @@ client = interactions.Client(intents=intents, description="Maple Bot")
 async def on_ready():
     # We can use the client "me" attribute to get information about the bot.
     print(f"We're online! We've logged in as {client.user.username}.")
+
 
 @interactions.slash_command(name='ask', description='Ask Grendel Bot a question',
                             scopes=env.discord_guild_id)
@@ -28,14 +27,19 @@ async def on_ready():
     required=False,
     opt_type=interactions.OptionType.INTEGER
 )
-async def askGrendel(ctx: interactions.SlashContext, question: interactions.OptionType.STRING, number_of_question_marks: interactions.OptionType.INTEGER = 1):
-    responses = ['', 'No', 'Whyyyy', 'Nani', 'Jes', 'Nani Kore', 'Watashi wa Kotowaru', 'Plz', 'Plz No', 'No Violins Plox', 'This is too difficult to answer']
-    expressions = ['(ㆆ _ ㆆ)', '⊂(◉‿◉)つ', '•`_´•', 'ʕっ•ᴥ•ʔっ', '(•_•) ( •_•)>⌐■-■ (⌐■_■)', '(｡◕‿‿◕｡)', '(︶︹︶)', '┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻', '(ง •̀_•́)ง', '(ノಠ益ಠ)ノ彡┻━┻', R'¯\_(ツ)_/¯']
+async def askGrendel(ctx: interactions.SlashContext, question: interactions.OptionType.STRING,
+                     number_of_question_marks: interactions.OptionType.INTEGER = 1):
+    responses = ['', 'No', 'Whyyyy', 'Nani', 'Jes', 'Nani Kore', 'Watashi wa Kotowaru', 'Plz', 'Plz No',
+                 'No Violins Plox', 'This is too difficult to answer']
+    expressions = ['(ㆆ _ ㆆ)', '⊂(◉‿◉)つ', '•`_´•', 'ʕっ•ᴥ•ʔっ', '(•_•) ( •_•)>⌐■-■ (⌐■_■)', '(｡◕‿‿◕｡)', '(︶︹︶)',
+                   '┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻', '(ง •̀_•́)ง', '(ノಠ益ಠ)ノ彡┻━┻', R'¯\_(ツ)_/¯']
 
     print(f'Command Message - ID: {ctx.message_id} -> {ctx.message}')
     # await ctx.send(f"{random.choice(responses)} {random.choice(expressions)}")
     await ctx.send(f"{question}{'?' * number_of_question_marks}")
-    await ctx.send(f"{random.choice(responses)} {random.choice(expressions)}")
+    message = await ctx.send(f"{random.choice(responses)} {random.choice(expressions)}")
+
+
 
 # Start the bot
 client.start(env.discord_bot_token)
