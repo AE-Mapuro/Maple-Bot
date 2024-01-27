f6e2a4f03e8bb40ee1a8f160522fb1acf98c6d7f
Eric Tsai <e.tsai70@gmail.com>
2024-01-26 21:33:56 -0500

    Changing cry toggle to use displayname and not username

--- src/exts/shenanigans/react.py
+++ src/exts/shenanigans/react.py
@@ -13,11 +13,11 @@ class React(Extension):
     async def react_startup(self):
         guild = await self.bot.fetch_guild(env.discord_guild_ids[0])
         for member in guild.members:
-            self.users[member.user.username] = False
+            self.users[member.user.display_name] = False
 
     @listen(MessageCreate)
     async def handle_message(self, event: MessageCreate):
-        if event.message.author.username in self.users and self.users[event.message.author.username]:
+        if event.message.author.display_name in self.users and self.users[event.message.author.display_name]:
             await event.message.add_reaction(emoji='<:bugcat_cry:886858534136987678>')
 
     @SC.shenanigans.subcommand(sub_cmd_name='cry',
@@ -27,15 +27,15 @@ class React(Extension):
                   opt_type=OptionType.USER,
                   required=False)
     async def toggle_react(self, ctx: SlashContext, user: User = None):
-        username = ctx.author.username if user is None else user.username
+        display_name = ctx.author.display_name if user is None else user.display_name
 
         # If the member to toggle doesn't exist, add them in
-        if username not in self.users:
-            self.users[username] = False
+        if display_name not in self.users:
+            self.users[display_name] = False
 
         # Toggle the saved flag
-        self.users[username] = True if not self.users[username] else False
-        await ctx.send(f'Reaction Toggle is now set to: {self.users[username]} for {username}')
+        self.users[display_name] = True if not self.users[display_name] else False
+        await ctx.send(f'Reaction Toggle is now set to: {self.users[display_name]} for {display_name}')
 
 
 def setup(bot):
