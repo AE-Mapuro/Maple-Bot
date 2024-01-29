d94c9f5cd2c4f9ebc171e6aff0f78023b9fb1654
Eric Tsai <e.tsai70@gmail.com>
2024-01-28 20:03:01 -0500

    Adding poll feature to Mapuro Bot

---
+++ src/exts/utilities/utility.py
@@ -0,0 +1,30 @@
+from interactions import *
+import resources.env as env
+
+
+def get_cmd_information():
+    return \
+        '''### `poll` - Create a poll 
+- `question` - Your question to everybody 
+- `choices` - Choices separated by g"|" and with "~" for each <emoji>~<choice> if a custom emoji is desired. 🔮~A|🎃~B|C. Limit 10'''
+
+
+class Utility(Extension):
+
+    @slash_command(name='util',
+                   description='Utility Commands for something useful perhaps',
+                   sub_cmd_name='info',
+                   sub_cmd_description='Info on available utility commands',
+                   scopes=env.discord_guild_ids)
+    async def info(self, ctx: SlashContext):
+        information = get_cmd_information()
+
+        embed = Embed(title='<a:bugcat_keyboard_smash:1201274359705239635> **Utility Commands Info** '
+                            '<a:bugcat_keyboard_smash:1201274359705239635>',
+                      description=information,
+                      color=Color.from_rgb(98, 242, 175))
+        await ctx.send(embeds=embed)
+
+
+def setup(bot):
+    Utility(bot)
