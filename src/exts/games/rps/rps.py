462499f3fcc70d73a3e9131e8baf1806db238ba9
Eric Tsai <e.tsai70@gmail.com>
2024-02-17 21:50:56 -0500

    Adding RPS Embed for the game

--- src/exts/games/rps/rps.py
+++ src/exts/games/rps/rps.py
@@ -9,6 +9,12 @@ class RockPaperScissors(Extension):
     def __init__(self, bot):
         self.games = {}
 
+    def build_game_embed(self) -> Embed:
+        embed = Embed(title="Let's Play Rock Paper Scissors!",
+                      description='Rock, Paper, or Scissors?',
+                      color=Color.from_rgb(255, 102, 255),
+                      timestamp=Timestamp.now())
+        return embed
 
 def setup(bot):
     RockPaperScissors(bot)
