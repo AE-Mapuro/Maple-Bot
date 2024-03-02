5443f9571cb254fb658e6cffdf5616a579c071b0
Eric Tsai <e.tsai70@gmail.com>
2024-03-01 21:34:42 -0500

    Adding Button component callback

--- src/exts/games/rps/rps.py
+++ src/exts/games/rps/rps.py
@@ -1,4 +1,6 @@
 from interactions import *
+from interactions.api.events import Component
+import random
 
 class RockPaperScissors(Extension):
     choices = ['rock', 'paper', 'scissor']
@@ -37,6 +39,39 @@ class RockPaperScissors(Extension):
                                "user_score": 0,
                                "bot_score": 0,
                                "tie": 0}
+
+    @listen(Component)
+    async def my_callback(self, event: Component):
+        ctx = event.ctx
+        user_id = str(ctx.user.id)
+        user_choice = ctx.custom_id
+        user_name = ctx.user.display_name
+
+        if user_id not in self.games:
+            self.add_new_player(user_id, user_name)
+
+        user_score = self.games.get(user_id, {}).get("user_score", 0)
+        bot_score = self.games.get(user_id, {}).get("bot_score", 0)
+        tie = self.games.get(user_id, {}).get("tie", 0)
+
+        bot_choice = random.choice(self.choices)
+
+        if bot_choice == user_choice:
+            tie += 1
+            self.games.get(user_id, {})["tie"] = tie
+            await ctx.send(f"Looks like we both chose {bot_choice}. Its a tie!")
+        elif (bot_choice == self.ROCK and user_choice == self.PAPER) or (
+                bot_choice == self.PAPER and user_choice == self.SCISSOR) or (
+                bot_choice == self.SCISSOR and user_choice == self.ROCK):
+            user_score += 1
+            self.games.get(user_id, {})["user_score"] = user_score
+            await ctx.send(f"I chose {bot_choice}, you chose {user_choice}. You won!")
+        else:
+            bot_score += 1
+            self.games.get(user_id, {})["bot_score"] = bot_score
+            await ctx.send(f"I chose {bot_choice}, you chose {user_choice}. You lost!")
+
+        self.games.get(user_id, {})["net_score"] = user_score - bot_score
 
 def setup(bot):
     RockPaperScissors(bot)
